7edc6d144ce1a62890daf71dc86326b44f7eb248
Steven Woo <swoo@tackable.com>
2013-11-11 13:32:30 -0800

    Initial Commit

---
+++ ImageCacheManager/ImageCacheManager.h
@@ -0,0 +1,13 @@
+//
+//  ImageCacheManager.h
+//  ImageCacheManager
+//
+//  Created by Steven Woo on 11/11/13.
+//  Copyright (c) 2013 Steven Woo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageCacheManager : NSObject
+
+@end
---
+++ ImageCacheManager/ImageCacheManager.m
@@ -0,0 +1,13 @@
+//
+//  ImageCacheManager.m
+//  ImageCacheManager
+//
+//  Created by Steven Woo on 11/11/13.
+//  Copyright (c) 2013 Steven Woo. All rights reserved.
+//
+
+#import "ImageCacheManager.h"
+
+@implementation ImageCacheManager
+
+@end
